4444d09cd33210050a7a06bd648ae8fe42b2c46f
Burak İpekci <burakipekci@outlook.com>
2023-02-07 15:34:57 +0300

    Patikadev practice
    created basic middleware

---
+++ MiddlewarePractice/MiddlewarePractice/Middlewares/Middleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MiddlewarePractice.Middlewares
+{
+    public class Middleware
+    {
+        RequestDelegate _next;
+        public Middleware(RequestDelegate next)
+        {
+            _next= next;
+        }
+        public async Task Invoke (HttpContext context)
+        {
+             System.Console.WriteLine("Hello Middleware.");
+            await _next.Invoke(context);
+            System.Console.WriteLine("By Middleware.");
+        }
+    }
+    static public class MiddlewareExtension
+    {
+        public static IApplicationBuilder UseMiddlewareTest(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<Middleware>();
+               
+        }
+    }
+}
---
+++ MiddlewarePractice/MiddlewarePractice/Startup.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewarePractice
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MiddlewarePractice", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MiddlewarePractice v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            //app.Run();
+            //app.Run(async context => Console.WriteLine("Middleware 1."));
+            //app.Run(async context => Console.WriteLine("Middleware 2."));
+
+
+            //app.Use(async (context, next) =>
+            //{
+            //    Console.WriteLine("Middleware 1 başladı.");
+            //    await next.Invoke(); Console.WriteLine("Middleware 1 Sonlandırılıyor.");
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    Console.WriteLine("Middleware 2 başladı.");
+            //    await next.Invoke(); Console.WriteLine("Middleware 2 Sonlandırılıyor.");
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    Console.WriteLine("Middleware 3 başladı.");
+            //    await next.Invoke(); Console.WriteLine("Middleware 3 Sonlandırılıyor.");
+            //});
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine("Use middleware tetiklendi");
+                await next.Invoke();
+            });
+            //app.Map();
+            app.Map("/example", internalApp => internalApp.Run(async context =>
+            {
+                Console.WriteLine("/example middleware tetiklendi");
+                await context.Response.WriteAsync("/example middleware tetiklendi.");
+            }));
+
+            //app.MapWhen
+            app.MapWhen(x => x.Request.Method == "GET", internalApp =>
+            {
+                internalApp.Run(async context =>
+                {
+                  
+                        Console.WriteLine("MapWhen middleware tetiklendi.");
+                         await context.Response.WriteAsync("MapWhen middleware tetiklendi.");    
+                });
+            });
+
+
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
